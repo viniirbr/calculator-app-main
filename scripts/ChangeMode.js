e073795084fd1e594b51dea085f187b8a4770fad
Vinicius Ribeiro <vayne99@gmail.com>
2022-02-23 21:28:07 -0300

    Responsividade adicionada

--- scripts/ChangeMode.js
+++ scripts/ChangeMode.js
@@ -4,10 +4,10 @@ export function changeMode(elementFromClass, counter) {
         var elementClasses = elementFromClass[i].className
         var regex;
         if (counter == 1) {
-            elementClasses = elementClasses.replace('theme3', `theme${counter}`)
+            elementClasses = elementClasses.replace(/theme3/g, `theme${counter}`)
             elementFromClass[i].className = elementClasses
         } else { 
-            regex = new RegExp(`theme${counter-1}`);
+            regex = new RegExp(`theme${counter-1}`, 'g');
             elementClasses = elementClasses.replace(regex, `theme${counter}`)
             elementFromClass[i].className = elementClasses
         }
